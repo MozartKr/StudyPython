ec1297d0d0716f1141f049799bab048f71963f9f
hc0517z <hc0517z@gmail.com>
2018-02-26 17:57:54 +0900

    생활코딩 - 문자와 데이터 타입

---
+++ Practice_2.py
@@ -0,0 +1,26 @@
+print("Hello")
+print('Hello')
+print("Hello 'world'")
+print('Hello "world"')
+
+print('Hello' + 'world')
+print('Hello'*3)
+print('Hello'[0])
+print('Hello'[1])
+print('Hello'[2])
+
+print('hello world'.capitalize())
+print('hello world'.upper())
+print('hello world'.__len__())
+print(len('hello world'))
+print('hello world'.replace('world', 'programming'))
+
+print("egoing's \"tutorial\"")
+print("\\")
+print("Hello\nworld")
+print("Hello\t\tworld")
+print("\a")
+print('Hello\nworld')
+
+print(10+5)
+print("10"+"5")
